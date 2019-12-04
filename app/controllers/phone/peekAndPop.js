fcb0cd5760fcbe33b05dfd096bd5241a8f3df906
Sergey Volkov <s.volkov@netris.ru>
2019-12-04 19:00:16 +0300

    fix: peekAndPop view

--- app/controllers/phone/peekAndPop.js
+++ app/controllers/phone/peekAndPop.js
@@ -1,17 +1,17 @@
 // Assign the 'pop' feature to the button
 $.button.previewContext = $.context;
 
-function actionTitle(e) {
+function alertActionTitle(e) {
     alert("Title: " + e.title + " / Style: " + e.style+" / Index: " + e.index);
 }
 
-function action(e) {
+function alertAction(e) {
     alert("Title: " + e.title + " / Style: " + e.style+" / Index: " + e.index);
 }
 
-function subAction(e) {
+function alertSubAction(e) {
     alert("Title: " + e.title + " / Style: " + e.style+" / Subindex: " + e.index);
-};
+}
 
 // Pop the preview
 function pop() {
